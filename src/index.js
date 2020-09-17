8c4eed2dcbe2a5bb7f186eb7159706a2daa94f8c
blackymax <maxmin3007@gmail.com>
2020-09-17 10:02:30 +0300

    feat: add solution for typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===undefined||array.length===0){
+    return 0
+  }
+  return Math.min(...array)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array===undefined||array.length===0){
+    return 0
+  }
+  return Math.max(...array)
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array===undefined||array.length===0){
+    return 0
+  }
+    result = (array.reduce(function(a,b){
+      return a+b})/array.length)
+      return result
+     
+      }
+   
+  
+
